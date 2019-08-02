5f2106b2e35572560cb20b3313eefe7906e60441
Heikki Mikkolainen <heikki.mikkolainen@gmail.com>
2019-08-02 20:48:32 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Scoresheet install script"""
+from setuptools import setup
+
+setup(
+    name="scoresheet",
+    version="1.1",
+    description="Create printable PDF scoresheet for kyykkä competition.",
+    py_modules=["scoresheet"],
+    entry_points={
+        "console_scripts": ["scoresheet=scoresheet:main"]
+    },
+    install_requires=['PyPDF2', 'appy']
+)
